ee36c5967b91e668e8dad53e4ff1e8636f6dc29e
Artit <zunnieartit26@gmail.com>
2019-09-05 12:24:34 +0700

    Add a full game

--- Node2D.gd
+++ Node2D.gd
@@ -1,36 +1,39 @@
 extends Control
 
-var story = "Hello! my name is %s\nMy last name is %s\nMy pet is %s\nI have %s baht"
+var story = "%s %s %s Once upon a time.You are a little boy or girl who tended a sheep farm form wolf.Everyone call you ... .Yeah! Your name is … and beloved by the villagers. But you're bored because there's nothing to play in the farm which have only sheeps.So you plan to run around the village While shouting ...You shout ... around the village.That annoys the villagers a lot.So they rushed and crunched you until you passed away.Game over"
 var questions = []
-var answers = ["ans1","ans2","ans3","ans4"]
-var pre1
-var pre2
-var pre3
-var pre4
+var question_number = 0
+var answers = []
 
 func _ready():
-	print(story % answers)
-	$VBoxContainer/DisplayText.text = "Hello there \nPlease press button to start"
-	
-	questions.append("Once upon a time. You are a little boy or girl who tended a sheep farm form wolf.\nEveryone call you")
+		
+	questions.append("Once upon a time. You are a little boy or girl who tended a sheep farm form wolf.\nEveryone call you...")
 	questions.append("Yeah Your name are … and beloved by the villagers. But you are bored because there are nothing to play in the farm which have only sheeps.So you plan to run around the village While shouting ...")
 	questions.append("You shout ... around the village.That annoys the villagers a lot.So they rushed and crunched you until you passed away.")
-	questions.append("Once upon a time.You are a little boy or girl who tended a sheep farm form wolf.Everyone call you …\n.Yeah! Your name is … and beloved by the villagers. But you're bored because there's nothing to play in the farm which have only sheeps.So you plan to run around the village\nWhile shouting ...\nYou shout ... around the village.That annoys the villagers a lot.So they rushed and crunched you until you passed away.\nGame over")
 	
 	
-	print(questions)
-	print(len(questions))
-	print(questions[0])
-	print(questions[1])
-	print(questions[2])
-	print(questions[3])
-	
-func _on_PlayerInput_focus_entered():
-	$PlayerInput.text = ""
-	
-func _on_PlayerButton_pressed():
 	$VBoxContainer/DisplayText.text = questions[0]
-	$"PlayerInput".text = pre1
-	if pre1 != "ans1":
+	$PlayerInput.text = ""
+
+func _on_PlayerButton_pressed():
+	if question_number == 0:
+		answers.append($PlayerInput.text)
+		print(answers)
 		$VBoxContainer/DisplayText.text = questions[1]
+		$PlayerInput.text = ""
+
 	
+	if question_number == 1:
+		answers.append($PlayerInput.text)
+		print(answers)
+		$VBoxContainer/DisplayText.text = questions[2]
+		$PlayerInput.text = ""
+
+	
+	if question_number == 2:
+		answers.append($PlayerInput.text)
+		print(answers)
+		$VBoxContainer/DisplayText.text = story % answers
+		$PlayerInput.text = ""
+
+	question_number = question_number + 1
